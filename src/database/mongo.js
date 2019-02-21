7cdc7343af1aad0ffbdff346da2c0db1fc8cef5f
EfrenSango <efren.sanchezgon@gmail.com>
2019-02-21 20:08:33 +0100

    DB on the cloud

--- src/database/mongo.js
+++ src/database/mongo.js
@@ -1,10 +1,9 @@
 const mongoose = require('mongoose');
 
-const URI = process.env.DB;
+const URI = process.env.DB; 
 
 module.exports = mongoose.connect(URI, { useNewUrlParser: true })
   .then(db => console.log('DB is connected'))
   .catch(err => console.log(err));
 
-
-// connections on port 27017
+ 
